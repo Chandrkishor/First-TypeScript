c16683849717b53ba0f4cdea54a701a0e183e528
Chandrakishor Tiwari <chandrkishor@mindrops.com>
2022-11-14 07:44:13 +0530

    typescript functions

--- myFunction.ts
+++ myFunction.ts
@@ -2,9 +2,26 @@
 function addTwo(num: number) {
    console.log(num+2)
 }
-
 // addTwo("5") // will gives an error just because we are passing string instead of number
 addTwo(5)
+
+
+function getUpper(val: string) {
+  return val.toUpperCase();
+}
+getUpper("tiwari")
+
+
+function singUpUser(name:string, email:string, isPaid:boolean) {}
+singUpUser("Ram","ram.email.com",true)
+
+let loginUser = (name:string, email:string, isPaid:boolean) => {}
+loginUser("tiwari", "email.com")
+
+
+
+
+
 
 
 export {}
