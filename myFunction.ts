68ff1ee754b69ec14468c4fe3349de60ab8eea00
Chandrakishor Tiwari <chandrkishor@mindrops.com>
2022-11-15 06:57:56 +0530

    function understating completed

--- myFunction.ts
+++ myFunction.ts
@@ -1,3 +1,6 @@
+
+//!NOTE to convert ts file into js cmd- "tsc filename.ts" then run node filename.js 
+
 //function
 function addTwo(num: number) {
    console.log(num+2)
@@ -11,17 +14,32 @@ function getUpper(val: string) {
 }
 getUpper("tiwari")
 
-
+//* normal functions
 function singUpUser(name:string, email:string, isPaid:boolean) {}
 singUpUser("Ram","ram.email.com",true)
 
+//* how to pass arguments
 let loginUser = (name:string, email:string, isPaid:boolean) => {}
-loginUser("tiwari", "email.com")
+// loginUser("tiwari", "email.com") // give an error that u are not providing isPaid argument
+ loginUser("tiwari", "email.com", true)
 
+ //* map function
+const heros=["saktimaan", "Hanuman", "Ram", "shiva"]
+heros.map((item): string => {
+  // return 1 // if we try to return 1 then it will give us error that we are returning No. instead of string 
+  return `Our heros are ${item}`
+})
+ 
+//* here function void not return any-thing. if we try to return anything then it will receive an error.
+function consoleError(error: string): void {
+  console.log(error)
+  // return "1"
+}
 
-
-
-
+//* here we are using never it's also never return a value as void but we can get/throw the error like this 
+function handleError(error: string): never {
+  throw new Error (error) // never return type means that the functions throw an exceptions or terminates execution of the program
+}
 
 
 export {}
